12ea6c57798b77cc4fd61f5cb3b5ba456c84b143
Eugene <setminsk@gmail.com>
2022-03-26 19:48:04 +0300

    TMS-6-added tests

--- src/test/java/DropDownTest.java
+++ src/test/java/DropDownTest.java
@@ -1,5 +1,28 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
 public class DropDownTest extends BaseTest {
 
+    @Test
+    public void dropdown() {
+        driver.get("https://the-internet.herokuapp.com/dropdown");
+        Select select = new Select(driver.findElement(By.id("dropdown")));
+        String firstOption = select.getOptions().get(0).getText();
+        assertEquals(firstOption,"Please select an option", "Error message");
+        String secondOption = select.getOptions().get(1).getText();
+        assertEquals(secondOption,"Option 1", "Error message");
+        String thirdOption = select.getOptions().get(2).getText();
+        assertEquals(thirdOption,"Option 2", "Error message");
+        assertEquals(select.getOptions().size(), 3, "Error elements");
+        assertFalse(select.getOptions().get(0).isEnabled());
+        select.selectByVisibleText("Option 1");
+        assertTrue(select.getOptions().get(1).isSelected());
+        select.selectByVisibleText("Option 2");
+        assertTrue(select.getOptions().get(2).isSelected());
+    }
 
 }
 
